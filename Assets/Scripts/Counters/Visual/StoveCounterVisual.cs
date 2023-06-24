6024808bf6a7d0c013a85eeb82f0c28da4751f16
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-06-24 21:07:44 +0200

    Stove visual

---
+++ Assets/Scripts/Counters/Visual/StoveCounterVisual.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StoveCounterVisual : MonoBehaviour {
+
+    [SerializeField] private ParticleSystem SizzlingParticles;
+    [SerializeField] private GameObject stoveOnVisual;
+
+
+    private StoveCounter stoveCounter;
+
+    private void Awake() {
+        stoveCounter = GetComponentInParent<StoveCounter>();
+        stoveCounter.OnRecipeChanged += OnRecipeChanged;
+    }
+
+    private void OnRecipeChanged(object sender, ValueChangedEvent<BaseRecipeSO> e) {
+        var recipe = e.NewValue as CookingRecipeSO;
+        if (recipe != null) {
+            TurnOn();
+        }
+        else {
+            TurnOff();
+        }
+    }
+
+    private void TurnOff() {
+        SizzlingParticles.gameObject.SetActive(false);
+        SizzlingParticles.Stop();
+
+        stoveOnVisual.SetActive(false);
+    }
+
+    private void TurnOn() {
+        SizzlingParticles.gameObject.SetActive(true);
+        SizzlingParticles.Play();
+
+        stoveOnVisual.SetActive(true);
+    }
+}
